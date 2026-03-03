7d29697f9c9dc64532c61dba0bb2a1f53ab74902
manishaj-code <manisha.j@crestinfosystems.com>
2026-03-03 18:03:13 +0530

    added new function and test cases

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,17 +1,12 @@
-from src.main import add
+from src.main import add, subtract
 
 def test_add_function():
     assert add(1, 2) == 3
-    assert add(1, -1) == -1
+    assert add(1, -1) == 0
     assert add(1, 0) == 1
-    assert add(1, 1) == 2
-    assert add(1, 2) == 3
-    assert add(1, 3) == 4
-    assert add(1, 4) == 5
-    assert add(1, 5) == 6
-    assert add(1, 6) == 7
-    assert add(1, 7) == 8
-    assert add(1, 8) == 9
-    assert add(0, 0) == 0
-    assert add(2, 3) == 5
-    assert add(5, 5) == 10
+
+def test_subtract_function():
+    assert subtract(1, 2) == -1
+    assert subtract(1, -1) == 2
+    assert subtract(1, 0) == 1
+    assert subtract(10, 5) == 5
