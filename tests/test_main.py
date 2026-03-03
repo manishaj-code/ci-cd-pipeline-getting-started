59fe56f5e6992b231c191edee65b2ea829e02231
manishaj-code <manisha.j@crestinfosystems.com>
2026-03-03 17:57:10 +0530

    checking test case failure

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,7 +2,7 @@ from src.main import add
 
 def test_add_function():
     assert add(1, 2) == 3
-    assert add(1, -1) == 0
+    assert add(1, -1) == -1
     assert add(1, 0) == 1
     assert add(1, 1) == 2
     assert add(1, 2) == 3
